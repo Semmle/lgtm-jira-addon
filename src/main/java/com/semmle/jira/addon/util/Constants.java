7c6043361390084d4f253ade9c7702761bda4fa6
David Verdeguer <david@semmle.com>
2019-02-25 17:36:27 +0100

    Add workflow related constants

--- src/main/java/com/semmle/jira/addon/util/Constants.java
+++ src/main/java/com/semmle/jira/addon/util/Constants.java
@@ -6,4 +6,12 @@ public class Constants {
 
   public static final String WORKFLOW_OPEN_STATUS_NAME = "Open";
   public static final String WORKFLOW_CLOSED_STATUS_NAME = "Closed";
+  public static final String WORKFLOW_SUPPRESSED_STATUS_NAME = "Suppressed";
+  public static final String WORKFLOW_FALSE_POSITIVE_STATUS_NAME = "False Positive";
+
+  public static final String WORKFLOW_REOPEN_TRANSITION_NAME = "Reopen";
+  public static final String WORKFLOW_CLOSE_TRANSITION_NAME = "Close";
+
+  public static final String WORKFLOW_SUPPRESS_TRANSITION_NAME = "Suppress";
+  public static final String WORKFLOW_UNSUPPRESS_TRANSITION_NAME = "Unsuppress";
 }
