bc5d932d87a4ca3245a67b5214301b944a9bf61f
David Verdeguer <david@semmle.com>
2019-03-04 17:34:19 +0100

    Load workflow properly in onPluginEnabled

--- src/main/java/com/semmle/jira/addon/config/init/PluginEnabledHandler.java
+++ src/main/java/com/semmle/jira/addon/config/init/PluginEnabledHandler.java
@@ -1,26 +1,26 @@
 package com.semmle.jira.addon.config.init;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
 import com.atlassian.event.api.EventListener;
 import com.atlassian.event.api.EventPublisher;
-import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.config.IssueTypeManager;
-import com.atlassian.jira.workflow.ConfigurableJiraWorkflow;
-import com.atlassian.jira.workflow.JiraWorkflow;
-import com.atlassian.jira.workflow.WorkflowUtil;
 import com.atlassian.plugin.Plugin;
 import com.atlassian.plugin.event.events.PluginEnabledEvent;
 import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
 import com.atlassian.plugin.spring.scanner.annotation.export.ExportAsService;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.opensymphony.workflow.FactoryException;
-import com.opensymphony.workflow.loader.WorkflowDescriptor;
-import java.io.IOException;
-import java.io.InputStream;
-import javax.inject.Inject;
-import javax.inject.Named;
-import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
+import com.semmle.jira.addon.util.Constants;
+import com.semmle.jira.addon.util.WorkflowUtils;
 
 @ExportAsService({PluginEnabledHandler.class})
 @Named("pluginEnabledHandler")
@@ -28,8 +28,6 @@ import org.springframework.beans.factory.InitializingBean;
 public class PluginEnabledHandler implements InitializingBean, DisposableBean {
 
   private static final String LGTM_PLUGIN_KEY = "com.semmle.lgtm-jira-addon";
-  private static final String ISSUE_TYPE_NAME = "LGTM alert";
-  private static final String WORKFLOW_NAME = "LGTM";
 
   @ComponentImport private final EventPublisher eventPublisher;
   @ComponentImport private final IssueTypeManager issueTypeManager;
@@ -51,48 +49,21 @@ public class PluginEnabledHandler implements InitializingBean, DisposableBean {
   }
 
   @EventListener
-  public void onPluginEnabled(PluginEnabledEvent event) {
+  public void onPluginEnabled(PluginEnabledEvent event) throws FactoryException, IOException {
     Plugin plugin = event.getPlugin();
     if (LGTM_PLUGIN_KEY.equals(plugin.getKey())) {
-      createLgtmIssueType(issueTypeManager);
       createLgtmWorkflow();
     }
   }
 
-  private static void createLgtmIssueType(IssueTypeManager issueTypeManager) {
-    try {
-      issueTypeManager.createIssueType(ISSUE_TYPE_NAME, "Issue type for managing LGTM alerts", 0l);
-    } catch (IllegalStateException e) {
-      // Issue Type already created
-    }
-  }
-
-  private static boolean createLgtmWorkflow() {
+  private static void createLgtmWorkflow() throws IOException, FactoryException {
     ClassLoader classLoader = PluginEnabledHandler.class.getClassLoader();
-    InputStream is = classLoader.getResourceAsStream("xml/LGTM.xml");
-    String xml = null;
-    try {
-      xml = IOUtils.toString(is);
-    } catch (IOException e) {
-      e.printStackTrace();
-      return false;
-    }
-    System.out.print(xml);
+    InputStream is = classLoader.getResourceAsStream("workflow/workflow.xml");
+    String workflowXml = IOUtils.toString(is);
 
-    WorkflowDescriptor wf = null;
-    try {
-      wf = WorkflowUtil.convertXMLtoWorkflowDescriptor(xml);
-    } catch (FactoryException e) {
-      e.printStackTrace();
-      return false;
-    }
-    JiraWorkflow w =
-        new ConfigurableJiraWorkflow(WORKFLOW_NAME, wf, ComponentAccessor.getWorkflowManager());
-    if (!ComponentAccessor.getWorkflowManager().workflowExists(w.getName())) {
-      ComponentAccessor.getWorkflowManager()
-          .createWorkflow(ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser(), w);
-      return true;
-    }
-    return false;
+    is = classLoader.getResourceAsStream("workflow/statuses.json");
+    String statusesJson = IOUtils.toString(is);
+
+    WorkflowUtils.createWorkflow(Constants.WORKFLOW_NAME, workflowXml, statusesJson);
   }
 }
