e8211b405622b55caef8150119bb491e3e5be0e9
Arthur Baars <arthur@semmle.com>
2019-03-15 11:38:55 +0100

    Close http connections and make request bodies more readable
    When connections are not closed properly the tests may block indefinitely.
    
    The tests now serialize to JSON and compute HMAC signatures automatically. This
    makes the tests a bit more easy to read and write.

--- src/test/java/it/com/semmle/jira/addon/IntegrationTest.java
+++ src/test/java/it/com/semmle/jira/addon/IntegrationTest.java
@@ -10,9 +10,10 @@ import com.atlassian.jira.testkit.client.restclient.Issue;
 import com.atlassian.jira.testkit.client.restclient.SearchRequest;
 import com.atlassian.jira.testkit.client.util.TestKitLocalEnvironmentData;
 import com.atlassian.jira.testkit.client.util.TimeBombLicence;
-import com.atlassian.jira.util.json.JSONException;
-import com.atlassian.jira.util.json.JSONObject;
 import com.google.common.collect.Iterables;
+import com.semmle.jira.addon.Request;
+import com.semmle.jira.addon.Request.Transition;
+import com.semmle.jira.addon.Response;
 import com.semmle.jira.addon.Util;
 import com.semmle.jira.addon.config.Config;
 import java.io.IOException;
@@ -25,9 +26,11 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.utils.HttpClientUtils;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
 public class IntegrationTest {
@@ -58,7 +61,7 @@ public class IntegrationTest {
   HttpClient httpClient;
   Config config;
 
-  public IntegrationTest() throws JSONException, ClientProtocolException, IOException {
+  public IntegrationTest() throws ClientProtocolException, IOException {
 
     // Use TestKit to initialise a test instance of Jira
     testKit = new Backdoor(new TestKitLocalEnvironmentData());
@@ -72,17 +75,18 @@ public class IntegrationTest {
   }
 
   @Test
-  public void testCreationAndTransitionOfIssues() throws IOException, JSONException {
-    // Create three issues, subsequent tests assume these have known IDs
-    Issue issueA = postWebhookJson(CREATE_STRING, CREATE_SIGNATURE);
+  public void testCreationAndTransitionOfIssues() throws IOException {
+    Request createRequest = Util.JSON.readValue(CREATE_STRING, Request.class);
+    // Create four issues, subsequent tests assume these have known IDs
+    Issue issueA = postWebhookJson(createRequest);
     assertEquals("IssueA id is wrong", "10000", issueA.id);
-    Issue issueB = postWebhookJson(CREATE_STRING, CREATE_SIGNATURE);
+    Issue issueB = postWebhookJson(createRequest);
     assertEquals("IssueB id is wrong", "10001", issueB.id);
-    Issue issueC = postWebhookJson(CREATE_STRING, CREATE_SIGNATURE);
+    Issue issueC = postWebhookJson(createRequest);
     assertEquals("IssueC id is wrong", "10002", issueC.id);
-    Issue issueD = postWebhookJson(CREATE_STRING, CREATE_SIGNATURE);
+    Issue issueD = postWebhookJson(createRequest);
     assertEquals("issueD id is wrong", "10003", issueD.id);
-    Issue issueE = postWebhookJson(CREATE_STRING, CREATE_SIGNATURE);
+    Issue issueE = postWebhookJson(createRequest);
     assertEquals("issueE id is wrong", "10004", issueE.id);
 
     // Check correct handling of text fields into issue
@@ -98,30 +102,18 @@ public class IntegrationTest {
     assertEquals(description, issueA.fields.description);
 
     // Close two issues
-    postWebhookJson(
-        "{\"issue-id\": \"10001\", \"transition\": \"close\" }",
-        "486fae456903f273033db2f8fc8c26a019f11f0e");
-    postWebhookJson(
-        "{\"issue-id\": \"10002\", \"transition\": \"close\" }",
-        "6bedc9b786705787c1522118f5723c94122d5200");
+    postWebhookJson(new Request(Transition.CLOSE, Long.valueOf(issueB.id)));
+    postWebhookJson(new Request(Transition.CLOSE, Long.valueOf(issueC.id)));
 
     // Reopen one issue
-    postWebhookJson(
-        "{\"issue-id\": \"10002\", \"transition\": \"reopen\" }",
-        "92bbd2be7524447cbd7d8f8e63c8eb7b88c29777");
+    postWebhookJson(new Request(Transition.REOPEN, Long.valueOf(issueC.id)));
 
     // Suppress two issues
-    postWebhookJson(
-        "{\"issue-id\": \"10003\", \"transition\": \"suppress\" }",
-        "f184bb5ba1853dc0715ba41e0ba1ae79d582d438");
-    postWebhookJson(
-        "{\"issue-id\": \"10004\", \"transition\": \"suppress\" }",
-        "3f7dfc9b322fc1da13c3e04f581422991d45e020");
+    postWebhookJson(new Request(Transition.SUPPRESS, Long.valueOf(issueD.id)));
+    postWebhookJson(new Request(Transition.SUPPRESS, Long.valueOf(issueE.id)));
 
     // Unsuppress one issue
-    postWebhookJson(
-        "{\"issue-id\": \"10004\", \"transition\": \"unsuppress\" }",
-        "d1e4593427491478c5a82bd87a3294cc84a33cf4");
+    postWebhookJson(new Request(Transition.UNSUPPRESS, Long.valueOf(issueE.id)));
 
     // Check states of all tickets
     assertEquals(issueA.fields.status.id(), getIssue("10000").fields.status.id()); // unchanged
@@ -140,17 +132,18 @@ public class IntegrationTest {
   }
 
   @Test
-  public void testManualTransitionOfIssues() throws IOException, JSONException {
-    // Create three issues, subsequent tests assume these have known IDs
-    Issue issueA = postWebhookJson(CREATE_STRING, CREATE_SIGNATURE);
+  public void testManualTransitionOfIssues() throws IOException {
+    // Create four issues, subsequent tests assume these have known IDs
+    Request createRequest = Util.JSON.readValue(CREATE_STRING, Request.class);
+    Issue issueA = postWebhookJson(createRequest);
     assertEquals("IssueA id is wrong", "10000", issueA.id);
-    Issue issueB = postWebhookJson(CREATE_STRING, CREATE_SIGNATURE);
+    Issue issueB = postWebhookJson(createRequest);
     assertEquals("IssueB id is wrong", "10001", issueB.id);
-    Issue issueC = postWebhookJson(CREATE_STRING, CREATE_SIGNATURE);
+    Issue issueC = postWebhookJson(createRequest);
     assertEquals("IssueC id is wrong", "10002", issueC.id);
-    Issue issueD = postWebhookJson(CREATE_STRING, CREATE_SIGNATURE);
+    Issue issueD = postWebhookJson(createRequest);
     assertEquals("issueD id is wrong", "10003", issueD.id);
-    Issue issueE = postWebhookJson(CREATE_STRING, CREATE_SIGNATURE);
+    Issue issueE = postWebhookJson(createRequest);
     assertEquals("issueE id is wrong", "10004", issueE.id);
 
     // Suppress 3 issues
@@ -175,20 +168,15 @@ public class IntegrationTest {
     assertEquals("Won't Fix", getIssue("10002").fields.resolution.name); // close suppressed
 
     // Send suppresion to close suppressed
-    postWebhookJson(
-        "{\"issue-id\": \"10002\", \"transition\": \"suppress\" }",
-        "ae581e61e472e9e1daf949085c8fc86b3f40cb68");
+    postWebhookJson(new Request(Transition.SUPPRESS, Long.valueOf(issueC.id)));
+
     // Should be unchanged
     assertEquals("Done", getIssue("10002").fields.status.name()); // close suppressed
     assertEquals("Won't Fix", getIssue("10002").fields.resolution.name); // close suppressed
 
     // Close suppressed issues
-    postWebhookJson(
-        "{\"issue-id\": \"10000\", \"transition\": \"close\" }",
-        "7a55296797b3db7ce9d3ce23c34d0fea8954a309");
-    postWebhookJson(
-        "{\"issue-id\": \"10002\", \"transition\": \"close\" }",
-        "6bedc9b786705787c1522118f5723c94122d5200");
+    postWebhookJson(new Request(Transition.CLOSE, Long.valueOf(issueA.id)));
+    postWebhookJson(new Request(Transition.CLOSE, Long.valueOf(issueC.id)));
 
     // Check closed issues
     assertEquals("Done", getIssue("10000").fields.status.name()); // closed
@@ -218,6 +206,7 @@ public class IntegrationTest {
     HttpResponse response = httpClient.execute(httpPut);
 
     assertEquals("Failed to configure plugin", 204, response.getStatusLine().getStatusCode());
+    HttpClientUtils.closeQuietly(response);
 
     return config;
   }
@@ -227,23 +216,21 @@ public class IntegrationTest {
     resp =
         httpClient.execute(
             new HttpDelete(baseUrl + "/rest/qr/1.0/plugin/enabled/com.semmle.lgtm-jira-addon"));
-    resp.getEntity().getContent().close();
+    HttpClientUtils.closeQuietly(resp);
 
     resp =
         httpClient.execute(
             new HttpPost(baseUrl + "/rest/qr/1.0/plugin/enabled/com.semmle.lgtm-jira-addon"));
-    resp.getEntity().getContent().close();
+    HttpClientUtils.closeQuietly(resp);
   }
 
-  private Issue postWebhookJson(String jsonString, String signature)
-      throws ClientProtocolException, IOException, JSONException {
-
+  private Issue postWebhookJson(Object request) throws ClientProtocolException, IOException {
+    byte[] payload = Util.JSON.writeValueAsBytes(request);
     HttpPost httpPost = new HttpPost(baseUrl + "/plugins/servlet/lgtm/webhook");
 
-    httpPost.setHeader("Content-type", "application/json");
-    httpPost.setHeader("X-LGTM-Signature", signature);
+    httpPost.setHeader("X-LGTM-Signature", Util.calculateHmac(config.getLgtmSecret(), payload));
 
-    httpPost.setEntity(new StringEntity(jsonString));
+    httpPost.setEntity(new ByteArrayEntity(payload, ContentType.APPLICATION_JSON));
 
     HttpResponse httpResponse = httpClient.execute(httpPost);
 
@@ -252,9 +239,10 @@ public class IntegrationTest {
         httpResponse.getStatusLine().getStatusCode(),
         isIn(Arrays.asList(200, 201)));
 
-    String responseJsonString =
-        new String(EntityUtils.toByteArray(httpResponse.getEntity()), StandardCharsets.UTF_8);
-    String issueId = new JSONObject(responseJsonString).getString("issue-id");
+    Response response = Util.JSON.readValue(httpResponse.getEntity().getContent(), Response.class);
+    String issueId = response.issueId;
+    HttpClientUtils.closeQuietly(httpResponse);
+
     return getIssue(issueId);
   }
 
