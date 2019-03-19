a9303dd61c6072333af8ba2926d7fb129eba392d
Nick Fyson <nickf@semmle.com>
2019-03-19 13:08:16 +0000

    Lock the custom field using managed configuration service

--- src/main/java/com/semmle/jira/addon/config/upgrades/UpgradeTask4CreateConfigKeyField.java
+++ src/main/java/com/semmle/jira/addon/config/upgrades/UpgradeTask4CreateConfigKeyField.java
@@ -1,6 +1,10 @@
 package com.semmle.jira.addon.config.upgrades;
 
 import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.config.managedconfiguration.ConfigurationItemAccessLevel;
+import com.atlassian.jira.config.managedconfiguration.ManagedConfigurationItem;
+import com.atlassian.jira.config.managedconfiguration.ManagedConfigurationItemBuilder;
+import com.atlassian.jira.config.managedconfiguration.ManagedConfigurationItemService;
 import com.atlassian.jira.issue.CustomFieldManager;
 import com.atlassian.jira.issue.context.GlobalIssueContext;
 import com.atlassian.jira.issue.fields.CustomField;
@@ -23,11 +27,14 @@ public class UpgradeTask4CreateConfigKeyField implements PluginUpgradeTask {
 
   private static final int BUILD_NUMER = 4;
   private PluginSettings settings;
+  private ManagedConfigurationItemService managedConfigurationItemService;
 
   @Autowired
   public UpgradeTask4CreateConfigKeyField(
-      @ComponentImport final PluginSettingsFactory pluginSettingsFactory) {
+      @ComponentImport final PluginSettingsFactory pluginSettingsFactory,
+      @ComponentImport final ManagedConfigurationItemService managedConfigurationItemService) {
     this.settings = pluginSettingsFactory.createGlobalSettings();
+    this.managedConfigurationItemService = managedConfigurationItemService;
   }
 
   @Override
@@ -47,6 +54,18 @@ public class UpgradeTask4CreateConfigKeyField implements PluginUpgradeTask {
                   "com.atlassian.jira.plugin.system.customfieldtypes:exacttextsearcher"),
               Collections.singletonList(GlobalIssueContext.getInstance()),
               Collections.singletonList(JiraUtils.getLgtmIssueType()));
+
+      ManagedConfigurationItem managedField;
+      ManagedConfigurationItemBuilder builder;
+
+      managedField = managedConfigurationItemService.getManagedCustomField(customField);
+      if (managedField != null) {
+        builder = ManagedConfigurationItemBuilder.builder(managedField);
+        builder.setManaged(true);
+        builder.setConfigurationItemAccessLevel(ConfigurationItemAccessLevel.LOCKED);
+        managedField = builder.build();
+        managedConfigurationItemService.updateManagedConfigurationItem(managedField);
+      }
 
       settings.put(Constants.CUSTOM_FIELD_CONFIG_KEY, customField.getIdAsLong().toString());
     }
